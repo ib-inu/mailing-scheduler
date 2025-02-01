b65f1146d309159599a85bb0f3a3712043537714
ibnu <ibnumuhthar9@gmail.com>
2025-02-01 17:10:08 +0530

    add: icons to dark toggle button

--- src/app/components/DarkModeToggle.tsx
+++ src/app/components/DarkModeToggle.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState, useEffect } from 'react';
+import { FiSun, FiMoon } from 'react-icons/fi';
 
 const DarkModeToggle = () => {
     const [darkMode, setDarkMode] = useState(false);
@@ -17,8 +18,8 @@ const DarkModeToggle = () => {
     };
 
     return (
-        <button onClick={toggleDarkMode} className="p-2 bg-gray-800 text-white rounded">
-            {darkMode ? 'Light Mode' : 'Dark Mode'}
+        <button onClick={toggleDarkMode} className="p-2 bg-gray-100 dark:bg-gray-800 dark:text-white rounded">
+            {darkMode ? <FiSun size={20} /> : <FiMoon size={20} />}
         </button>
     );
 };
